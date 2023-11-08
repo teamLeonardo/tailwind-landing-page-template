c57be6ff52c0aa041776ea59af43725154d3fad3
leonardo sifuentes <leonardosm3.14@gmail.com>
2023-11-08 15:45:59 -0500

    init hero change

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,16 +5,12 @@ export const metadata = {
 
 import Hero from '@/components/hero'
 import Features from '@/components/features'
-import Testimonials from '@/components/testimonials'
-import Newsletter from '@/components/newsletter'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
-      <Testimonials />
-      <Newsletter />
     </>
   )
 }
